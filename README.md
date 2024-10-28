ba51dddb8f0322c0a2642bcd3f93e0a5e90d70cc
Magnus Lindgren <magnus@newseed.se>
2024-10-28 18:27:18 +0100

    Added first version of example code

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ This is a repository for NewSeeds challange for the the Ideon Coder Kids 2024.
 ## Links
 
 https://www.npmjs.com/package/node-poweredup
+
+
+## Assignment 1
+
+Make the car run a track by programming the commands for movement and calling them in the right order.
+
+## Assignment 2
+
+Create event based steering using arrow keys and steer the car.
---
+++ car.js
@@ -0,0 +1,35 @@
+// car.js
+
+class Car {
+
+    constructor(motorA, motorB, motorSteering) {
+        this.motorA = motorA;
+        this.motorB = motorB;
+        this.motorSteering = motorSteering;
+    }
+
+    turnLeft() {
+        console.log('Turning left');
+        // Add logic to turn left using motors
+    }
+
+    turnRight() {
+        console.log('Turning right');
+        // Add logic to turn right using motors
+    }
+
+    moveForward() {
+        console.log('Moving forward');
+        // Add logic to move forward using motors
+    }
+
+    moveBackward() {
+        console.log('Moving backward');
+        // Add logic to move backward using motors
+    }
+
+    stop() {
+        console.log('Stopping');
+        // Add logic to stop the car
+    }
+}
