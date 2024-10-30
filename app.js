acafd812aab1ba1a46c952be794215c89a5153ed
newseed <newseed@newseed.se>
2024-10-30 19:56:26 +0100

    Start of assignment

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Import modules
 import PoweredUP from "inimi-poweredup";
 import {Car} from "./car.js";
 
@@ -7,16 +8,71 @@ let found = false;
 // Modify for each machine
 let searchMac1 = '90:84:2B:6D:76:41'; // 1
 let searchMac2 = '90:84:2B:7D:10:F0'; // 2
-let searchMac3 = '90:84:2B:6D:76:41'; // 3
-let searchMac = searchMac1;
+let searchMac3 = '90:84:2B:6F:9D:0A'; // 3
+let searchMac = searchMac2;
+// Use gobal variables to make these available in the program
+let car = null;
+let hub = null;
 
-poweredUP.on("discover", async (data) => { // Wait to discover a Hub
-    // get the hub
-    let hub = data.hub;
-    // get mac address from hub
-    let hubMac = (await data.peripheral.getAddress()).toUpperCase();
-    console.log(`Discovered ${hub.name} at `+hubMac+`!`);
-    if ((hub.name == "Technic Hub") && (hubMac == searchMac)) {
+// Setup on keypress
+let keypress = (await import('keypress')).default;
+keypress(process.stdin);
+
+// listen for the "keypress" event
+process.stdin.on('keypress', function (ch, key) {
+    try{
+        // TODO: Use the command below to check which key is pressed
+        //console.log('key pressed: ', key);
+        if (key && key.ctrl && key.name == 'c') {
+            if (hub){
+                console.log("Disconnecting!");
+                hub.disconnect();
+            }
+            process.exit(0);
+        }
+        else if (car){
+            if (key.name == 'up'){
+                // car move forward
+                // TODO: call a function in the car object
+            }
+            if (key.name == 'left'){
+                // car turn left
+                // TODO: call a function in the car object
+            }
+            if (key.name == 'right'){
+                // car turn right
+                // TODO: call a function in the car object
+            }
+            if (key.name == 'down'){
+                // car move backwards
+                // TODO: call a function in the car object
+            }
+            if (key.name == 's'){
+                // car stop
+                // TODO: call a function in the car object
+            }
+            if (key.name == 'c'){
+                // car run course
+                // TODO: call a function in the car object
+            }
+        }
+    }
+    catch(e){
+        console.log(e);
+    }
+});
+process.stdin.setRawMode(true);
+process.stdin.resume();
+
+// Setup the connection to a Lego car
+poweredUP.on("discover", async (data) => {
+    // Get the hub
+    hub = data.hub;
+    // Get mac address from hub
+    let mac = (await data.peripheral.getAddress()).toUpperCase();
+    // Only connect to the correct car
+    if ((hub.name == "Technic Hub") && (mac == searchMac)) {
+        console.log(`Discovered ${hub.name} at `+mac+`!`);
         console.log("Found mathing hub");
         await hub.connect(); // Connect to the Hub
         console.log("Connected");
@@ -30,11 +86,9 @@ poweredUP.on("discover", async (data) => { // Wait to discover a Hub
         console.log(hub.batteryLevel);
         // Stop scanning for new devices
         poweredUP.stop();
-        // TODO: add code to control motors
-        const car = new Car(motorA, motorB, motorSteering);
-
-        car.runCourse();
-
+        // Initialize the car
+        car = new Car(hub, motorA, motorB, motorSteering);
+        car.setZero();
     }
 });
 poweredUP.scan(); // Start scanning for Hubs
--- car.js
+++ car.js
@@ -2,39 +2,79 @@
 
 export class Car {
 
-    constructor(motorA, motorB, motorSteering) {
+    constructor(hub,motorA, motorB, motorSteering) {
+        this.hub = hub;
         this.motorA = motorA;
         this.motorB = motorB;
         this.motorSteering = motorSteering;
+        this.speed = 0;
+        this.angle = 0;
+        this.sendCommand = false; // avoid multiple speed changes
     }
 
-    runCourse() {
+    async runCourse() {
         console.log('Running course');
-        // Add logic to run the course
+        // Add logic to run the course - example logic below
+        // TODO: Call the functions in the correct order and use wait in between.
     }
 
     turnLeft() {
         console.log('Turning left');
         // Add logic to turn left using motors
+        // TODO: Use this.gotoAngle();
     }
 
     turnRight() {
         console.log('Turning right');
         // Add logic to turn right using motors
+        // TODO: Use this.gotoAngle();
     }
 
-    moveForward() {
+    async moveForward() {
         console.log('Moving forward');
         // Add logic to move forward using motors
+        // TODO: Use this.gotoAngle();
+        // TODO: Use this.setSpeed();
     }
 
-    moveBackward() {
+    async moveBackward() {
         console.log('Moving backward');
         // Add logic to move backward using motors
+        // TODO: Use this.setSpeed();
     }
 
-    stop() {
+    async stop() {
         console.log('Stopping');
         // Add logic to stop the car
+        // TODO: Use this.setSpeed();
     }
+
+    /* Internal functions - do not edit these until you know what you are doing */
+
+    async sleep(time){
+        await this.hub.sleep(time);
+    }
+
+    async setSpeed(speed){
+        if ((this.speed != speed)&&(!this.sendCommand)){
+            this.sendCommand = true;
+            this.speed = speed;
+            await this.motorB.setPower(speed);
+            await this.motorA.setPower(speed);
+            this.sendCommand = false;
+        }
+    }
+    async gotoAngle(angle){
+        if ((this.angle != angle)&&(!this.sendCommand)){
+            this.angle = angle;
+            this.sendCommand = true;
+            await this.motorSteering.gotoAngle(angle);
+            this.sendCommand = false;
+        }
+    }
+
+    async setZero(){
+        await this.motorSteering.resetZero();
+    }
+
 }
