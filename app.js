c4bf2d1ecb020e35c6077122191a14d7eb4fe0e2
newseed <newseed@newseed.se>
2024-10-30 19:40:59 +0100

    Suggested solution

--- app.js
+++ app.js
@@ -8,32 +8,65 @@ let found = false;
 let searchMac1 = '90:84:2B:6D:76:41'; // 1
 let searchMac2 = '90:84:2B:7D:10:F0'; // 2
 let searchMac3 = '90:84:2B:6F:9D:0A'; // 3
-let searchMac = searchMac1;
+let searchMac = searchMac2;
+let car = null;
+let hub = null;
 
-let keyboard = (await import('keyboard')).default;
+let keypress = (await import('keypress')).default;
 // make `process.stdin` begin emitting "keypress" events
 keypress(process.stdin);
 
 // listen for the "keypress" event
 process.stdin.on('keypress', function (ch, key) {
-    console.log('key pressed: ', key);
-    if (key && key.ctrl && key.name == 'c') {
-        process.stdin.pause();
-        process.exit(0);
+    try{
+        //console.log('key pressed: ', key);
+        if (key && key.ctrl && key.name == 'c') {
+            if (hub){
+                console.log("Disconnecting!");
+                hub.disconnect();
+            }
+            process.stdin.pause();
+            process.exit(0);
+        }
+        else if (car){
+            if (key.name == 'up'){
+                car.moveForward();
+            }
+            if (key.name == 'left'){
+                car.gotoAngle(-90);
+            }
+            if (key.name == 'right'){
+                car.gotoAngle(90);
+            }
+            if (key.name == 'down'){
+                car.setSpeed(-50);
+            }
+            if (key.name == 's'){
+                car.setSpeed(0);
+            }
+            if (key.name == 'c'){
+                car.runCourse();
+            }
+        }
+    }
+    catch(e){
+        console.log(e);
     }
 });
+process.stdin.setRawMode(true);
+process.stdin.resume();
 
 poweredUP.on("discover", async (data) => { // Wait to discover a Hub
     // get the hub
-    let hub = data.hub;
+    hub = data.hub;
     // get mac address from hub
-    let hubMac = (await data.peripheral.getAddress()).toUpperCase();
-    console.log(`Discovered ${hub.name} at `+hubMac+`!`);
-    if ((hub.name == "Technic Hub") && (hubMac == searchMac)) {
+    let mac = (await data.peripheral.getAddress()).toUpperCase();
+    if ((hub.name == "Technic Hub") && (mac == searchMac)) {
+        console.log(`Discovered ${hub.name} at `+hubMac+`!`);
         console.log("Found mathing hub");
         await hub.connect(); // Connect to the Hub
         console.log("Connected");
-        console.log("Setting up motors...");
+        console.log("Setting up motors...");18
         console.log("Checking motor A...")
         const motorA = await hub.waitForDeviceAtPort("A");
         console.log("Checking motor B...")
@@ -44,8 +77,8 @@ poweredUP.on("discover", async (data) => { // Wait to discover a Hub
         // Stop scanning for new devices
         poweredUP.stop();
         // TODO: add code to control motors
-        const car = new Car(motorA, motorB, motorSteering);
-        car.runCourse();
+        car = new Car(hub, motorA, motorB, motorSteering);
+        car.setZero();
     }
 });
 
--- car.js
+++ car.js
@@ -2,49 +2,95 @@
 
 export class Car {
 
-    constructor(motorA, motorB, motorSteering) {
+    constructor(hub,motorA, motorB, motorSteering) {
+        this.hub = hub;
         this.motorA = motorA;
         this.motorB = motorB;
         this.motorSteering = motorSteering;
         this.speed = 0;
         this.angle = 0;
+        this.sendCommand = false; // avoid multiple speed changes
     }
 
-    runCourse() {
+    async runCourse() {
         console.log('Running course');
-        // Add logic to run the course
+        // Add logic to run the course - example logic below
+        this.moveForward();
+        console.log('Check 1');
+        await this.sleep(1000);
+        console.log('Check 2');
+        this.turnLeft();
+        await this.sleep(1000);
+        console.log('Check 3');
+        this.moveForward();
+        await this.sleep(1000);
+        console.log('Check 4');
+        this.turnRight();
+        await this.sleep(1000);
+        console.log('Check 5');
+        this.moveForward();
+        await this.sleep(500);
+        this.stop();
+        console.log('Finish!');
     }
 
     turnLeft() {
         console.log('Turning left');
         // Add logic to turn left using motors
+        this.gotoAngle(-90);
     }
 
     turnRight() {
         console.log('Turning right');
         // Add logic to turn right using motors
+        this.gotoAngle(90);
     }
 
-    moveForward() {
+    async moveForward() {
         console.log('Moving forward');
         // Add logic to move forward using motors
+        await this.gotoAngle(0);
+        await this.setSpeed(50);
     }
 
-    moveBackward() {
+    async moveBackward() {
         console.log('Moving backward');
         // Add logic to move backward using motors
+        await this.setSpeed(-50);
     }
 
-    stop() {
+    async stop() {
         console.log('Stopping');
         // Add logic to stop the car
+        this.setSpeed(0);
     }
 
-    setSpeed(speed){
-        this.motorA.setSpeed(speed);
-        this.motorB.setSpeed(speed);
+    /* Internal functions - do not edit these until you know what you are doing */
+
+    async sleep(time){
+        await this.hub.sleep(time);
     }
-    setAngle(angle){
-        this.motorSteering.gotoAngle(angle);
+
+    async setSpeed(speed){
+        if ((this.speed != speed)&&(!this.sendCommand)){
+            this.sendCommand = true;
+            this.speed = speed;
+            await this.motorB.setPower(speed);
+            await this.motorA.setPower(speed);
+            this.sendCommand = false;
+        }
     }
+    async gotoAngle(angle){
+        if ((this.angle != angle)&&(!this.sendCommand)){
+            this.angle = angle;
+            this.sendCommand = true;
+            await this.motorSteering.gotoAngle(angle);
+            this.sendCommand = false;
+        }
+    }
+
+    async setZero(){
+        await this.motorSteering.resetZero();
+    }
+
 }
